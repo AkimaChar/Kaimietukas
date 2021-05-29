819fe3f6cafea277088167b77de0df43847abaf7
Nikita <chip_05@inbox.ru>
2021-05-29 19:24:42 +0300

    29.05.2021 19:25

--- script/dist/script.dev.js
+++ script/dist/script.dev.js
@@ -3,6 +3,22 @@
 window.addEventListener('scroll', function () {
   var h_bg = document.getElementById("bgToScroll");
   h_bg.style.backgroundPositionY = window.pageYOffset - 420 + "px";
-  h_bg.style.transform = "translateY(" + window.pageYOffset * 0.1 + "px)";
+  h_bg.style.transform = "translateY(" + window.pageYOffset * -0.07 + "px)";
+  h_bg.style.marginTop = window.pageYOffset * -0.1 + "px";
+
+  if (window.pageYOffset != 0) {
+    document.getElementById("header").style.backgroundColor = "#ffffffff";
+    this.document.querySelector(".logo__text").style.color = "#000";
+    var elements = document.querySelectorAll(".nav-bar > a:not(:first-of-type)");
+    Array.prototype.forEach.call(elements, function (el, i) {
+      el.style.color = "black";
+    });
+  } else if (window.pageYOffset == 0) {
+    document.getElementById("header").style.backgroundColor = "#ffffff00";
+    this.document.querySelector(".logo__text").style.color = "#ffffffff";
+    var elements = document.querySelectorAll(".nav-bar > a:not(:first-of-type)");
+    Array.prototype.forEach.call(elements, function (el, i) {
+      el.style.color = "white";
+    });
+  }
 });
-window.addEventListener('scroll', function () {});

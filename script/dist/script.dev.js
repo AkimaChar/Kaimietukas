13b9e066ec35053cde4f01b02e8fadc135cee937
Valeria Dmitrievna <dev.valeriadmitrievna@gmail.com>
2021-06-01 16:11:31 +0300

    Home page desktop

--- script/dist/script.dev.js
+++ script/dist/script.dev.js
@@ -1,11 +1,6 @@
 "use strict";
 
 window.addEventListener('scroll', function () {
-  var h_bg = document.getElementById("bgToScroll");
-  h_bg.style.backgroundPositionY = window.pageYOffset - 420 + "px";
-  h_bg.style.transform = "translateY(" + window.pageYOffset * -0.07 + "px)";
-  h_bg.style.marginTop = window.pageYOffset * -0.1 + "px";
-
   if (window.pageYOffset != 0) {
     document.getElementById("header").style.backgroundColor = "#ffffffff";
     this.document.querySelector(".logo__text").style.color = "#000";

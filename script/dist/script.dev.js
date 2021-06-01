02e3136a77abcd974c1a0efcc706cbc9ba7de495
Valeria Dmitrievna <dev.valeriadmitrievna@gmail.com>
2021-06-01 18:18:44 +0300

    Finish desktop

--- script/dist/script.dev.js
+++ script/dist/script.dev.js
@@ -1,19 +1,74 @@
 "use strict";
 
-window.addEventListener('scroll', function () {
+function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _nonIterableSpread(); }
+
+function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance"); }
+
+function _iterableToArray(iter) { if (Symbol.iterator in Object(iter) || Object.prototype.toString.call(iter) === "[object Arguments]") return Array.from(iter); }
+
+function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = new Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } }
+
+window.addEventListener("scroll", function () {
   if (window.pageYOffset != 0) {
     document.getElementById("header").style.backgroundColor = "#ffffffff";
     this.document.querySelector(".logo__text").style.color = "#000";
-    var elements = document.querySelectorAll(".nav-bar > a:not(:first-of-type)");
-    Array.prototype.forEach.call(elements, function (el, i) {
+    var elements = document.querySelectorAll(".nav-bar > a:not(.nav-bar__button--active)");
+
+    _toConsumableArray(elements).forEach(function (el) {
       el.style.color = "black";
     });
   } else if (window.pageYOffset == 0) {
     document.getElementById("header").style.backgroundColor = "#ffffff00";
     this.document.querySelector(".logo__text").style.color = "#ffffffff";
-    var elements = document.querySelectorAll(".nav-bar > a:not(:first-of-type)");
-    Array.prototype.forEach.call(elements, function (el, i) {
+
+    var _elements = document.querySelectorAll(".nav-bar > a:not(.nav-bar__button--active)");
+
+    _toConsumableArray(_elements).forEach(function (el) {
       el.style.color = "white";
     });
   }
 });
+var anchors = document.querySelectorAll("a.scroll-to");
+var _iteratorNormalCompletion = true;
+var _didIteratorError = false;
+var _iteratorError = undefined;
+
+try {
+  var _loop = function _loop() {
+    var anchor = _step.value;
+    anchor.addEventListener("click", function (e) {
+      e.preventDefault();
+      var elements = document.querySelectorAll(".nav-bar > a");
+
+      _toConsumableArray(elements).forEach(function (el) {
+        el.classList.remove("nav-bar__button--active");
+        el.classList.add("nav-bar__button");
+      });
+
+      e.target.classList.add("nav-bar__button--active");
+      e.target.style.color = "#ffffffff";
+      var blockID = anchor.getAttribute("href");
+      document.querySelector(blockID).scrollIntoView({
+        behavior: "smooth",
+        block: "start"
+      });
+    });
+  };
+
+  for (var _iterator = anchors[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+    _loop();
+  }
+} catch (err) {
+  _didIteratorError = true;
+  _iteratorError = err;
+} finally {
+  try {
+    if (!_iteratorNormalCompletion && _iterator["return"] != null) {
+      _iterator["return"]();
+    }
+  } finally {
+    if (_didIteratorError) {
+      throw _iteratorError;
+    }
+  }
+}
--- script/script.js
+++ script/script.js
@@ -1,19 +1,43 @@
-window.addEventListener('scroll', function () {
-    if (window.pageYOffset != 0) {
-        document.getElementById("header").style.backgroundColor = "#ffffffff";
-        this.document.querySelector(".logo__text").style.color = "#000"
-        var elements = document.querySelectorAll(".nav-bar > a:not(:first-of-type)");
-        Array.prototype.forEach.call(elements, function (el, i) {
-            el.style.color = "black";
-        });
-    } else if (window.pageYOffset == 0) {
-        document.getElementById("header").style.backgroundColor = "#ffffff00";
-        this.document.querySelector(".logo__text").style.color = "#ffffffff"
-        var elements = document.querySelectorAll(".nav-bar > a:not(:first-of-type)");
-        Array.prototype.forEach.call(elements, function (el, i) {
-            el.style.color = "white";
-        });
-    }
-})
+window.addEventListener("scroll", function () {
+  if (window.pageYOffset != 0) {
+    document.getElementById("header").style.backgroundColor = "#ffffffff";
+    this.document.querySelector(".logo__text").style.color = "#000";
+    var elements = document.querySelectorAll(
+      ".nav-bar > a:not(.nav-bar__button--active)"
+    );
+    [...elements].forEach((el) => {
+      el.style.color = "black";
+    });
+  } else if (window.pageYOffset == 0) {
+    document.getElementById("header").style.backgroundColor = "#ffffff00";
+    this.document.querySelector(".logo__text").style.color = "#ffffffff";
+    const elements = document.querySelectorAll(
+      ".nav-bar > a:not(.nav-bar__button--active)"
+    );
+    [...elements].forEach((el) => {
+      el.style.color = "white";
+    });
+  }
+});
 
+const anchors = document.querySelectorAll("a.scroll-to");
 
+for (let anchor of anchors) {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const elements = document.querySelectorAll(".nav-bar > a");
+    [...elements].forEach((el) => {
+      el.classList.remove("nav-bar__button--active");
+      el.classList.add("nav-bar__button");
+    });
+    e.target.classList.add("nav-bar__button--active");
+    e.target.style.color = "#ffffffff";
+
+    const blockID = anchor.getAttribute("href");
+    document.querySelector(blockID).scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  });
+}
